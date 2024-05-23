df25db498330c8ee974d215c957866bba29d9caf
AlgernonHolmes <nicolas.alarcon.a@usach.cl>
2024-05-23 02:09:33 -0400

    [feat] config-server implemented

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# AutofixBackendMS
-Project contains all the files for the AutoFix backend using micro-services.
+# AutoFixBackendMS: a micro-service webapp
---
+++ config-server/src/test/java/com/servicems/msconfigserv/MsconfigservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicems.msconfigserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsconfigservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
