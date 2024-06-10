236ca625f12f09eeb59d81b086d8b22ba4887bd1
AlgernonHolmes <nicolas.alarcon.a@usach.cl>
2024-06-10 18:26:34 -0400

    [update] dockerfiles added to each sub project

--- repair-ms/src/main/java/com/repair/ms/repairms/RepairMsApplication.java
+++ repair-ms/src/main/java/com/repair/ms/repairms/RepairMsApplication.java
@@ -2,8 +2,11 @@ package com.repair.ms.repairms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RepairMsApplication {
 
     public static void main(String[] args) {
--- repairlist-ms/src/main/java/com/repairlist/ms/repairlistms/RepairlistMsApplication.java
+++ repairlist-ms/src/main/java/com/repairlist/ms/repairlistms/RepairlistMsApplication.java
@@ -2,8 +2,11 @@ package com.repairlist.ms.repairlistms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RepairlistMsApplication {
 
 	public static void main(String[] args) {
