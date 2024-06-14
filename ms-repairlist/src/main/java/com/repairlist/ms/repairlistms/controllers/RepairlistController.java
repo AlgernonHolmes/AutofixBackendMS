d39407df2c179e9d57f8b3dbe6312661d693e49b
AlgernonHolmes <nicolas.alarcon.a@usach.cl>
2024-06-14 00:25:22 -0400

    [update] yaml files updated

--- ms-repairlist/src/main/java/com/repairlist/ms/repairlistms/controllers/RepairlistController.java
+++ ms-repairlist/src/main/java/com/repairlist/ms/repairlistms/controllers/RepairlistController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ms/repairlist")
+@RequestMapping("/ms/listrepair")
 @CrossOrigin("*")
 public class RepairlistController {
 
